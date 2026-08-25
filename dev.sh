8939d53a14977758513a45d7fbf16fb56e5b4170
merhasmukh <mhgn0001@gmail.com>
2026-08-25 17:21:21 +0530

    add

---
+++ dev.sh
@@ -0,0 +1,15 @@
+sudo apt update
+sudo apt install -y \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libncurses5-dev \
+  libncursesw5-dev \
+  libffi-dev \
+  liblzma-dev \
+  tk-dev \
+  uuid-dev \
+  wget
